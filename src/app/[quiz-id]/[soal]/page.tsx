cd21a7c146ed122109e55d7cfe6cf1834dbcc9cc
Rafi Ananta <rafianantan@gmail.com>
2025-09-18 21:12:25 +0700

    m

--- src/app/[quiz-id]/[soal]/page.tsx
+++ src/app/[quiz-id]/[soal]/page.tsx
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export default function QuizSoalPageServer({ params }: { params: { "quiz-id": string; soal: string } }) {
   const cookieStore = cookies();
-  // @ts-ignore
+  // @ts-expect-error
   const token = typeof cookieStore.get === "function" ? cookieStore.get("token")?.value || "" : "";
   const quizId = params["quiz-id"];
   const soalId = params["soal"];
