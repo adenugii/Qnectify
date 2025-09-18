8b4d693010d69a3d151b23b872905ea4b5472448
Rafi Ananta <rafianantan@gmail.com>
2025-09-18 21:16:21 +0700

    m

--- src/app/[quiz-id]/[soal]/page.tsx
+++ src/app/[quiz-id]/[soal]/page.tsx
@@ -1,10 +1,9 @@
 import QuizSoalClient from "@/components/quiz/QuizSoalClient";
 import { cookies } from "next/headers";
 
-export default function QuizSoalPageServer({ params }: { params: { "quiz-id": string; soal: string } }) {
+export default async function QuizSoalPageServer({ params }: { params: { "quiz-id": string; soal: string } }) {
   const cookieStore = cookies();
-  // @ts-expect-error
-  const token = typeof cookieStore.get === "function" ? cookieStore.get("token")?.value || "" : "";
+  const token = typeof (await cookieStore).get === "function" ? (await cookieStore).get("token")?.value || "" : "";
   const quizId = params["quiz-id"];
   const soalId = params["soal"];
   return <QuizSoalClient quizId={quizId} soalId={soalId} token={token} />;
